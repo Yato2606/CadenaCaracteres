9553ea93df09f67a103fe7a2ce9376deb5054f7f
Yato2606 <david.quevedo@uabc.edu.mx>
2022-04-06 23:26:20 -0700

    Creacion de Conversion

---
+++ Conversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            int num1 = 5, num2, resultado;
+
+            //preguntamos al usuario el numero
+            Console.WriteLine("Dame un numero para sumarlo: ");
+            //se asigna la variable de tipo string
+            cadena = Console.ReadLine();
+            num2 = Int32.Parse(cadena);
+            //num2 = Convert.ToInt32(cadena);
+            //Realizar la suma
+            resultado = num1 + num2;
+            Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
